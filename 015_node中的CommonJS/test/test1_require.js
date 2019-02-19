f4b5ee1c75c32bb71148b046764ea13140a4c047
whynotgonow <whynotgonow@163.com>
2019-02-19 20:57:32 +0800

    fix err

--- 015_node中的CommonJS/test/test1_require.js
+++ 015_node中的CommonJS/test/test1_require.js
@@ -6,7 +6,7 @@ let counter = require('./test1')
 
 // 直接调用
 console.log(counter)          // { temp: [Function] }
-counter.printNextCount();     // TypeError: counter.printNextCount is not a function
+counter.temp.printNextCount();     // TypeError: counter.temp.printNextCount is not a function
 
 // new一个对象再调用
 let obj = new counter()       // TypeError: counter is not a constructor
